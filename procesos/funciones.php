de9ec54dcc07041a06c3e6bbed0f562803b111fd
thyrons <willynarvaez6@gmail.com>
2015-07-14 12:17:31 -0500

    CAMBIOS
    REPORTES VENTA

--- procesos/funciones.php
+++ procesos/funciones.php
@@ -1,8 +1,23 @@
-<?php
+<?php	
 	function maxCaracter($texto, $cant){        
-    	$texto = substr($texto, 0,$cant);
+    	$texto = substr($texto, 0,$cant);    	
     	return $texto;
 	}
+
+	function ceil_caracter($texto, $cant){        		
+		$array_t = array();		
+		$var = 0;
+		$total = strlen($texto) / $cant;				
+		$total = ceil($total);		
+		for($i = 0; $i < $total; $i++){						
+    		$array_t[$i] = substr($texto, $var,$cant);    		    		
+    		$var = $var + $cant;    		
+
+    	}
+		return $array_t;
+    	
+	}
+
 	function truncateFloat($number, $digitos){
 	    $raiz = 10;
 	    $multiplicador = pow ($raiz,$digitos);
